7f0f9d12c35063d2d314ceb4e869c5f7b658ca5f
luorenjie <lrj193927@gmail.com>
2021-01-14 08:40:33 -0500

    add PaymentStateChangeInterceptor

---
+++ src/main/java/org/northernjay/msscssm/services/PaymentStateChangeInterceptor.java
@@ -0,0 +1,35 @@
+package org.northernjay.msscssm.services;
+
+import lombok.RequiredArgsConstructor;
+import org.northernjay.msscssm.domain.Payment;
+import org.northernjay.msscssm.domain.PaymentEvent;
+import org.northernjay.msscssm.domain.PaymentState;
+import org.northernjay.msscssm.repositories.PaymentRepository;
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
+import org.springframework.statemachine.transition.Transition;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class PaymentStateChangeInterceptor extends StateMachineInterceptorAdapter<PaymentState, PaymentEvent> {
+
+    private final PaymentRepository paymentRepository;
+
+    @Override
+    public void preStateChange(State<PaymentState, PaymentEvent> state, Message<PaymentEvent> message,
+                               Transition<PaymentState, PaymentEvent> transition, StateMachine<PaymentState, PaymentEvent> stateMachine) {
+        Optional.ofNullable(message).ifPresent(msg -> {
+            Optional.ofNullable((Long) msg.getHeaders().getOrDefault(PaymentServiceImpl.PAYMENT_ID_HEADER, -1L))
+                    .ifPresent(paymentId -> {
+                        Payment payment = paymentRepository.getOne(paymentId);
+                        payment.setState(state.getId());
+                        paymentRepository.save(payment);
+                    });
+        });
+    }
+}
